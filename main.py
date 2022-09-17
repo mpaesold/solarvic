ad20b8a7315a32430b336775b0fa1c4ec22d3880
Martin Paesold <mpaesold@users.noreply.github.com>
2022-09-17 15:35:50 +0800

    File output OS independent
    CSV file is written to folder called "output" in current directory.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import Settings
 import DailyWACOutput as dwaco
 import numpy as np
+import os
 
 def main():
     dailywacouput = dwaco.DailyWACOutput(
@@ -9,7 +10,10 @@ def main():
             Settings.nrel_api_key, 180, 10, 31, 15)
 
     dwaco.scan_azimuths_tilts(dailywacouput)
-    dwaco.write_average_ac_to_csv(dailywacouput)
+    outfile = os.path.join( os.path.curdir, 'output',
+                           'out_lot{}_long{}.csv'.format(Settings.lat,
+                                                         Settings.long) )
+    dwaco.write_average_ac_to_csv( dailywacouput, outfile )
     return 0
 
 if __name__ == "__main__":
