38f28551f488488c214ad4237694ad882fc79a09
Martin Paesold <mpaesold@users.noreply.github.com>
2022-09-21 21:42:04 +0800

    Import consumption
    Converted datetime in consumption.csv such that it can be converted to numpy datetime
    
    Imported raw consumption

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import os
 
 
 def main():
-    infile = os.path.join(os.path.curdir, 'input', 'pv_config.csv')
-    data = np.loadtxt(infile, skiprows=1)
+    # Import data
+    pv_config = os.path.join(os.path.curdir, 'input', 'pv_config.csv')
+    data = np.loadtxt(pv_config, skiprows=1)
     long = data[0, 0]
     lat = data[0, 1]
     azimuth_tilts = data[:, 2:4]
     areas = data[:, 4]
+    holidays_file = os.path.join(os.path.curdir, 'input', 'holidays.csv')
+    holidays = np.genfromtxt(holidays_file, usecols=(0), dtype='datetime64[D]',
+                             skip_header=1, delimiter=',')
+    consumption_file = os.path.join(os.path.curdir, 'input', 'consumption.csv')
+    consumption = np.genfromtxt(consumption_file, usecols=(1, 2, 6, 7),
+                                dtype=('datetime64[m]', int, float, 'U4'), 
+                                skip_header=1, delimiter=',')
 
+    # Output files
     outfile = os.path.join(os.path.curdir, 'output',
                            'out_lat{}_long{}.csv'.format(lat, long))
     outfileplot = os.path.join(os.path.curdir, 'output',
@@ -21,24 +30,27 @@ def main():
     print('Azimuths and tilts:\n', azimuth_tilts)
     print('Areas:\n', areas)
 
-    dailywacouput = dwaco.DailyWACOutput(
-        lat, long,
-        Settings.nrel_api_key, azimuth_tilts)
+    print(holidays)
+    print(consumption)
 
-    print('Scan azimuth and tilt settings:')
-    dwaco.scan_azimuths_tilts(dailywacouput)
-    # dwaco.write_average_ac_to_csv( dailywacouput, outfile )
-    pout_hourly = dwaco.calculate_power_output_hourly(dailywacouput, areas)
-    pout_daily = dwaco.calculate_power_output_daily(pout_hourly)
-    pout_monthly = dwaco.calculate_power_output_monthly(pout_daily)
+    #dailywacouput = dwaco.DailyWACOutput(
+    #    lat, long,
+    #    Settings.nrel_api_key, azimuth_tilts)
 
-    pp(np.arange(12) + 1, 'Hourly power production per month:', format='{}')
-    for idx in range(24):
-        pp(pout_hourly[idx, :])
-    pp(pout_daily, 'Daily power production:')
-    pp(pout_monthly, 'Monthly power production:', format='{:.0f}')
+    #print('Scan azimuth and tilt settings:')
+    #dwaco.scan_azimuths_tilts(dailywacouput)
+    ## dwaco.write_average_ac_to_csv( dailywacouput, outfile )
+    #pout_hourly = dwaco.calculate_power_output_hourly(dailywacouput, areas)
+    #pout_daily = dwaco.calculate_power_output_daily(pout_hourly)
+    #pout_monthly = dwaco.calculate_power_output_monthly(pout_daily)
 
-    dwaco.plot_hourly_power_output(pout_hourly, outfileplot)
+    #pp(np.arange(12) + 1, 'Hourly power production per month:', format='{}')
+    #for idx in range(24):
+    #    pp(pout_hourly[idx, :])
+    #pp(pout_daily, 'Daily power production:')
+    #pp(pout_monthly, 'Monthly power production:', format='{:.0f}')
+
+    #dwaco.plot_hourly_power_output(pout_hourly, outfileplot)
     return 0
 
 def pp(outputs, title='', format='{:.2f}'):
